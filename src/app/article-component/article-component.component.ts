8a31a2eb1821c1171df62a42531429f0ec6a0d21
Thien Nguyen <thien.ngu25@gmail.com>
2024-09-25 15:35:20 +0200

    add binding

--- src/app/article-component/article-component.component.ts
+++ src/app/article-component/article-component.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Article } from './article';
 
 @Component({
   selector: 'app-article-component',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './article-component.component.html',
   styleUrl: './article-component.component.scss',
 })
-export class ArticleComponentComponent {}
+export class ArticleComponentComponent {
+  name: string = 'bob';
+  sayHello(): void {
+    alert('Bonjour !');
+  }
+
+  article: Article = {
+    title: "Titre de l'article",
+    author: 'John Doe',
+    content: "Voici le contenu de l'article.",
+    image: 'https://via.placeholder.com/350x150',
+    isPublished: true,
+    comment: '',
+  };
+
+  togglePublication(): void {
+    this.article.isPublished = !this.article.isPublished;
+  }
+}
