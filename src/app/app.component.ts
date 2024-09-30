2fc53b746d8143791cf4eb348918b538f549839d
Thien Nguyen <thien.ngu25@gmail.com>
2024-09-30 14:43:18 +0200

    add router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponentComponent } from './article-component/article-component.component';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './home-page/home-page.component';
 
 interface Article {
   title: string;
@@ -16,48 +17,15 @@ interface Article {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ArticleComponentComponent, CommonModule],
+  imports: [
+    RouterOutlet,
+    ArticleComponentComponent,
+    CommonModule,
+    HomePageComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Thien';
-
-  articles = [
-    {
-      title: 'Angular 16: Les nouveautés',
-      author: 'Alice',
-      content: "Les nouveautés d'Angular 16 incluent...",
-      image: 'https://via.placeholder.com/350x150',
-      isPublished: true,
-      comment: '',
-      likes: 120,
-    },
-    {
-      title: 'Développer une API REST',
-      author: 'Bob',
-      content: 'Développer une API REST nécessite...',
-      image: 'https://via.placeholder.com/350x150',
-      isPublished: false,
-      comment: '',
-      likes: 75,
-    },
-    {
-      title: 'Pourquoi TypeScript est essentiel ?',
-      author: 'Charlie',
-      content: 'TypeScript apporte de la robustesse...',
-      image: 'https://via.placeholder.com/350x150',
-      isPublished: true,
-      comment: '',
-      likes: 200,
-    },
-  ];
-
-  visibleArticles: number = 0;
-
-  constructor() {
-    this.visibleArticles = this.articles.filter(
-      (article) => article.isPublished
-    ).length;
-  }
 }
--- src/app/article-component/article-component.component.ts
+++ src/app/article-component/article-component.component.ts
@@ -1,28 +1,45 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Article } from './article';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-article-component',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './article-component.component.html',
   styleUrl: './article-component.component.scss',
 })
 export class ArticleComponentComponent {
   name: string = 'bob';
 
-  article: Article = {
-    title: 'Baseball',
-    author: 'John Doe',
-    content:
-      'Le baseball se joue avec deux équipes qui alternent en défense et en attaque. Neuf joueurs se trouvent sur le terrain en défense : un lanceur, un receveur et sept autres joueurs qui sont en défense répartis sur le terrain pour rattraper la balle, si possible de volée pour éliminer le batteur.',
-    image: 'baseball.png',
-    isPublished: true,
-    comment: '',
-  };
+  article: Article[] = [
+    {
+      id: 1,
+      title: 'Baseball',
+      author: 'John Doe',
+      content:
+        'Le baseball se joue avec deux équipes qui alternent en défense et en attaque. Neuf joueurs se trouvent sur le terrain en défense : un lanceur, un receveur et sept autres joueurs qui sont en défense répartis sur le terrain pour rattraper la balle, si possible de volée pour éliminer le batteur.',
+      image: 'baseball.png',
+      isPublished: true,
+      comment: '',
+    },
+    {
+      id: 2,
+      title: 'Baseball',
+      author: 'John Doe',
+      content:
+        'Le baseball se joue avec deux équipes qui alternent en défense et en attaque. Neuf joueurs se trouvent sur le terrain en défense : un lanceur, un receveur et sept autres joueurs qui sont en défense répartis sur le terrain pour rattraper la balle, si possible de volée pour éliminer le batteur.',
+      image: 'baseball.png',
+      isPublished: true,
+      comment: '',
+    },
+  ];
 
-  togglePublication(): void {
-    this.article.isPublished = !this.article.isPublished;
+  togglePublication(articleId: number): void {
+    const article = this.article.find((a) => a.id === articleId);
+    if (article) {
+      article.isPublished = !article.isPublished;
+    }
   }
 }
