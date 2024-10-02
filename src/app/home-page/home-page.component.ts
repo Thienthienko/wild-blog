87091071aa9954037ed5ed28e99346805d780877
Thien Nguyen <thien.ngu25@gmail.com>
2024-10-02 12:00:23 +0200

    output

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ArticlePageComponent } from '../article-page/article-page.component';
+import { Article } from '../models/article';
 
 @Component({
   selector: 'app-home-page',
@@ -11,7 +12,13 @@ import { ArticlePageComponent } from '../article-page/article-page.component';
   styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
-  articles = [
+  messageFromChild: string = '';
+
+  handleNotification(message: string) {
+    this.messageFromChild = message;
+  }
+
+  articles: Article[] = [
     {
       id: 1,
       title: 'Angular 16: Les nouveautés',
@@ -21,6 +28,7 @@ export class HomePageComponent {
       isPublished: true,
       comment: '',
       likes: 120,
+      isLiked: false,
     },
     {
       id: 2,
@@ -31,6 +39,7 @@ export class HomePageComponent {
       isPublished: true,
       comment: '',
       likes: 75,
+      isLiked: false,
     },
     {
       id: 3,
@@ -41,6 +50,7 @@ export class HomePageComponent {
       isPublished: true,
       comment: '',
       likes: 200,
+      isLiked: false,
     },
   ];
 
